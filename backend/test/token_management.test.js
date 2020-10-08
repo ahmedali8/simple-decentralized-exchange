024c6b5a74929590592e9974bf92932485c41b3b
ahmedali8 <imahmedalibhatti@gmail.com>
2020-10-08 17:47:09 +0500

    test2: Assert if it properly update token address

--- backend/test/token_management.test.js
+++ backend/test/token_management.test.js
@@ -17,4 +17,12 @@ contract('token_management', async (accounts) => {
 
         address.should.equal('0x69c4BB240cF05D51eeab6985Bab35527d04a8C64');
     });
+
+    it('should update supported token address', async () => {
+        await sender.addNewToken('OPEN', '0x3472059945ee170660a9A97892a3cf77857Eba3A');
+        
+        let address = await sender.tokens.call('OPEN');
+
+        address.should.equal('0x3472059945ee170660a9A97892a3cf77857Eba3A');
+    });
 });
