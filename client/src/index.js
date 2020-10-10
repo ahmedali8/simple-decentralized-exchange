fe46deb72a240cc6d844d5524a1c3ba3e26bf407
ahmedali8 <imahmedalibhatti@gmail.com>
2020-10-10 22:43:25 +0500

    added bulma import

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bulma-start/css/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
