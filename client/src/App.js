03a57239191c610a6559cebf30db645ab41a0add
ahmedali8 <imahmedalibhatti@gmail.com>
2020-10-11 21:16:38 +0500

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { GlobalProvider } from './contextAPI/GlobalState';
+import { useStore } from './contextAPI/GlobalState';
+import Nav from './Components/Nav';
+import Description from './Components/Description';
+import Container from './Components/Container';
+import InstallMetaMask from './Components/InstallMetaMask';
 
 import './App.css';
 
 function App() {
+
+    const [state] = useStore();
+    console.log(state)
+
+    const content = (
+        <div>
+            <Nav />
+            <Description />
+            <Container />
+        </div>
+    );
+
     return (
-        <GlobalProvider>
-            Hello World
-        </GlobalProvider>
+        <div>
+            {
+                state.web3 ?
+                    content :
+                    <InstallMetaMask />
+            }
+        </div>
     );
 }
 
