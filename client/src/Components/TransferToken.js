86eb4d787d3657ffb748dd95639104eb62b9e3c8
ahmedali8 <imahmedalibhatti@gmail.com>
2020-10-11 11:17:54 +0500

    dispatched closeTransfer function

--- client/src/Components/TransferToken.js
+++ client/src/Components/TransferToken.js
@@ -2,38 +2,48 @@
 // It takes the address, amount to transfer and gas limit
 
 import React from 'react';
+import { useStore } from '../contextAPI/GlobalState';
+import { closeTransfer } from '../store/actions';
 import InputField from './InputField';
 
-function TransferToken(props) {
+
+function TransferToken() {
+
+    const [state, dispatch] = useStore();
+
+    const handleClick = () => {
+        dispatch(closeTransfer());
+    }
+
     return (
         <div className="panel-block is-paddingless is-12">
             <div className="column is-12" id="token-lists">
 
-                <InputField onInputChangeUpdateField={props.onInputChangeUpdateField}
-                    fields={props.fields} name="receiver" placeholder="Receiver Address" />
+                <InputField onInputChangeUpdateField={state.onInputChangeUpdateField}
+                    fields={state.fields} name="receiver" placeholder="Receiver Address" />
 
 
-                <InputField onInputChangeUpdateField={props.onInputChangeUpdateField}
-                    fields={props.fields} name="amount" placeholder="Amount To Transfer" addon={props.transferDetail.symbol.toUpperCase()} />
+                <InputField onInputChangeUpdateField={state.onInputChangeUpdateField}
+                    fields={state.fields} name="amount" placeholder="Amount To Transfer" addon={state.transferDetail.symbol.toUpperCase()} />
 
-                <InputField onInputChangeUpdateField={props.onInputChangeUpdateField}
-                    default={props.defaultGasPrice}
-                    fields={props.fields} name="gasPrice" placeholder="Gas Price In Gwei" addon="Gas Price(gwei)" />
+                <InputField onInputChangeUpdateField={state.onInputChangeUpdateField}
+                    default={state.defaultGasPrice}
+                    fields={state.fields} name="gasPrice" placeholder="Gas Price In Gwei" addon="Gas Price(gwei)" />
 
-                <InputField onInputChangeUpdateField={props.onInputChangeUpdateField}
-                    default={props.defaultGasLimit}
-                    fields={props.fields} name="gasLimit" placeholder="Gas Limit" addon="Gas Limit" />
+                <InputField onInputChangeUpdateField={state.onInputChangeUpdateField}
+                    default={state.defaultGasLimit}
+                    fields={state.fields} name="gasLimit" placeholder="Gas Limit" addon="Gas Limit" />
 
                 <div className="field is-grouped is-pulled-right">
                     <p className="control">
-                        <a className="button is-light" disabled={props.inProgress}
-                            onClick={() => props.closeTransfer()}>
+                        <a className="button is-light" disabled={state.inProgress}
+                            onClick={() => handleClick()}>
                             Back
                         </a>
                     </p>
                     <p className="control">
-                        <a className={props.inProgress ? "button is-danger is-loading" : "button is-danger"}
-                            disabled={props.inProgress} onClick={() => props.Transfer()}>
+                        <a className={state.inProgress ? "button is-danger is-loading" : "button is-danger"}
+                            disabled={state.inProgress} onClick={() => state.Transfer()}>
                             Transfer
                         </a>
                     </p>
